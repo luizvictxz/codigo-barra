ec355821d4640173212762212be19d6c061ef86c
Luiz <luiz.victor9973@gmail.com>
2025-12-04 22:26:47 -0300

    change if in extract

--- extract_id.c
+++ extract_id.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    // Requisito 1: Validação de argumento obrigatório
+    // Validação de argumento obrigatório
     if (argc < 2)
     {
         printf("Arquivo inválido (Argumento ausente).\n");
@@ -14,23 +14,21 @@ int main(int argc, char *argv[])
     char identificador[9]; // 8 dígitos + null
     int resultado = ean8_extract_id(argv[1], identificador);
 
-    // Requisito 2: Validar arquivo inexistente ou inválido
+    // Validar arquivo inexistente ou inválido
     if (resultado == -1)
     {
         printf("Arquivo inválido!\n");
         return 1;
     }
-    // Requisito 3: Validar se código foi encontrado
-    else if (resultado == -2)
+    // Validar se código foi encontrado
+    if (resultado == -2)
     {
         printf("Código não encontrado.\n");
         return 0; // O enunciado não especifica return code para erro lógico, mas 0 é safe
     }
-    // Requisito 4: Sucesso -> Saída padrão
-    else
-    {
-        printf("%s\n", identificador);
-    }
+    // Sucesso -> Saída padrão
+
+    printf("%s\n", identificador);
 
     return 0;
 }
